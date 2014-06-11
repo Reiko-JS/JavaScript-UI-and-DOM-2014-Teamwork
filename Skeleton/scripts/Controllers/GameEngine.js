db50df9e4ff89293cdfbd21b5a29e6ea51bfcb90
stiliankanchev <pechi169@gmail.com>
2014-06-11 22:26:30 +0300

    Mousedown and mousemove events update mouse coordinates

--- Skeleton/scripts/Controllers/GameEngine.js
+++ Skeleton/scripts/Controllers/GameEngine.js
@@ -11,9 +11,21 @@ define(function(require) {
     function GameEngine(stage, gameFieldOptions) {
         var background = new Background(stage, gameFieldOptions);
         background.draw();
+		var mouseEvents = new MouseEventHandler();
 
+        attachEvents(mouseEvents);
         //fruitDrawer = new FruitDrawer(stage);
         // ...
+    }
+	
+	function attachEvents(mouseEvents) {
+        window.onmousemove = function (event) {
+            mouseEvents.updateCoords(event, mouseEvents, 500);
+        };
+        window.onmousedown = function (event) {
+            mouseEvents.mouseDown(event, mouseEvents);
+        };
+        window.onmouseup = function (event) { mouseEvents.mouseUp(event, mouseEvents); };
     }
 
     function addFruit() {
--- Skeleton/scripts/Controllers/MouseEventHandler.js
+++ Skeleton/scripts/Controllers/MouseEventHandler.js
@@ -1,12 +1,43 @@
-define(function(require) {
+define(function (require) {
 
-    // Empty Constructor
-    function MouseEventHandler() {}
+    function MouseEventHandler() {
+        this.x = null;
+        this.y = null;
+        this.isMouseDown = false;
+    }
 
-    MouseEventHandler.prototype.getCoords = function() {
-        var coords = null;
+    MouseEventHandler.prototype.mouseDown = function (event, mouseObj) {
+        mouseObj.isMouseDown = true;
+        //console.log(mouseObj.isMouseDown);
+    }
 
-        // ...
+    MouseEventHandler.prototype.mouseUp = function (event, mouseObj) {
+        mouseObj.isMouseDown = false;
+        //console.log(mouseObj.isMouseDown);
+    };
+
+    MouseEventHandler.prototype.updateCoords = function (event, mouseObj, updateInterval) {
+        if (mouseObj.isMouseDown) {
+            event = event || window.event;
+            mouseObj.x = event.clientX;
+            mouseObj.y = event.clientY;
+            //console.dir(mouseObj.getCoords());
+        }
+    };
+
+    MouseEventHandler.prototype.getCoords = function () {
+        if (this.x == null || this.y == null) {
+            throw {
+                message: 'Coordinates are not set yet',
+                name: 'NullCoordinatesException'
+            }
+        }
+
+        var coords = (function (x, y) {
+            return { x: x, y: y }
+        })(this.x, this.y);
+
+        console.log(coords);
 
         return coords;
     };
