3700d0ec3b4774cc182f737815a8934ebb2144da
kirilt7 <kirilt7@gmail.com>
2014-06-19 09:55:07 +0300

    SVG background added

--- FruitNinja/scripts/main.js
+++ FruitNinja/scripts/main.js
@@ -4,7 +4,6 @@ define(function(require) {
     var GameEngine = require('scripts/Controllers/GameEngine.js'),
         Polyfill = require('scripts/Helper/Polyfill.js'),
         EventListener = require('scripts/Controllers/EventListener.js'),
-        Background = require('scripts/Models/Background.js'),
         GameSettings = require('scripts/Models/GameSettings.js');
 
     // Provides requestAnimationFrame in a cross browser way.
@@ -15,6 +14,4 @@ define(function(require) {
     var eventListener = new EventListener(gameEngine, GameSettings.eventListenerOptions);
     eventListener.setButtonEvents();
 
-    var background = new Background(GameSettings.stage, GameSettings.gameFieldOptions);
-    background.draw();
 });
